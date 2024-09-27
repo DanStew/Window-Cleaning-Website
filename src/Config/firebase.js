f90c0ed79f14c3cf43b7e46d481b97740ee4bf0c
DanStew <danielstewarts2004@gmail.com>
2024-09-27 21:08:43 +0100

    Initialising the project, adding some images that we have

---
+++ src/Config/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAW6wxY493_kaSTeavTc60sV2MFjRHqVvo",
+  authDomain: "window-cleaning-website-7ee1f.firebaseapp.com",
+  projectId: "window-cleaning-website-7ee1f",
+  storageBucket: "window-cleaning-website-7ee1f.appspot.com",
+  messagingSenderId: "82979618009",
+  appId: "1:82979618009:web:c159dd003ad9e232c612c8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
