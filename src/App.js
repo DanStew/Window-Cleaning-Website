ab69f2a86c28509c81dc769f19bff8c193099334
DanStew <danielstewarts2004@gmail.com>
2024-09-27 21:15:14 +0100

    Setting up routing the pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+//Importing the workings of react-router-dom
+import { Route,Routes,BrowserRouter as Router } from "react-router-dom";
+
+//Importing the pages
+import Home from "./Pages/Home";
+import Services from "./Pages/Services";
+import Contact from "./Pages/Contact";
+
 function App() {
   return (
     <div className="App">
-      
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/Services" element={<Services />} />
+            <Route path="/Contact" element={<Contact />} />
+          </Routes>
+      </Router>
     </div>
   );
 }
