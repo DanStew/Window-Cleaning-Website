1d367b21e1d1b2ea86d74901b2987ce9a2bc1e10
DanStew <danielstewarts2004@gmail.com>
2024-10-03 22:54:36 +0100

    Finishing the Contact Us page

--- src/Components/ContactUs.jsx
+++ src/Components/ContactUs.jsx
@@ -5,7 +5,8 @@ function ContactUs({order}){
     return(
         <div className={"contactUs flexDisplay column heightGap " + order}>
             <p className="title">Contact Us</p>
-            <p className="bigSubheading mainText">Please use the contact information or contact form below if you any questions about the services we provide or would want a Free, No Obligation Quote</p>
+            <p className="bigSubheading mainText">If you live in Farham or Surrounding areas and have any questions about our services or would like a Free, No obligation quotation, please contact us using the information or form below : </p>
+            <MapAPIImplementation className="map"/>
             <div className="contactInfo flexItems flexDisplay column heightGap">
                 <div className="contactRow flexDisplay flexItems">
                     <p className="heading flexItems">Email : </p>
@@ -15,7 +16,6 @@ function ContactUs({order}){
                     <p className="heading flexItems">Phone Number : </p>
                     <p className="heading flexItems">01252722665</p>
                 </div>
-                <MapAPIImplementation className="map"/>
                 <button onClick={() => window.open("https://www.facebook.com/profile.php?id=100054523986920")} className="flexDisplay smallGap"><img src={facebookLogo} alt="Facebook Logo" /><p className="bigSubheading">View our Facebook</p></button>
             </div>
         </div>
