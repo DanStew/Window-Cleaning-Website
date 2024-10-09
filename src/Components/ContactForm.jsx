67454a9ddbbaedf3b697a83fa93315f5e75e436d
DanStew <danielstewarts2004@gmail.com>
2024-10-09 16:34:36 +0100

    Implementing sending emails from the website

--- src/Components/ContactForm.jsx
+++ src/Components/ContactForm.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react"
 import { closeContactForm } from "../Functions/contactFormFunctions"
+import SendEmail from "../Functions/emailFunctions"
 
 function ContactForm(){
 
@@ -8,6 +9,18 @@ function ContactForm(){
     const [phoneNmb,setPhoneNmb] = useState("")
     const [title,setTitle] = useState("")
     const [message,setMessage] = useState("")
+    const [errorMsg,setErrorMsg] = useState("")
+
+    function preSendEmail(){
+        setErrorMsg(SendEmail(name,email,phoneNmb,title,message))
+
+        //Resetting the values of the inputs
+        setName("")
+        setEmail("")
+        setPhoneNmb("")
+        setTitle("")
+        setMessage("")
+    }
 
     return(
         <div id="contactForm" className="contactForm notActive flexItems">
@@ -22,11 +35,12 @@ function ContactForm(){
             <div className="contactFormForm">
                 <form className="flexDisplay column gap" action="#">
                     <input type="text" placeholder="Enter your Name..." value={name} onChange={(e) => setName(e.target.value)}/>
-                    <input type="text" placeholder="Enter your Email..." value={email} onChange={(e) => setEmail(e.target.value)}/>
+                    <input type="email" placeholder="Enter your Email..." value={email} onChange={(e) => setEmail(e.target.value)} required/>
                     <input type="text" placeholder="Enter your Phone Number..." value={phoneNmb} onChange={(e) => setPhoneNmb(e.target.value)}/>
                     <input type="text" placeholder="Enter the Email Title..." value={title} onChange={(e) => setTitle(e.target.value)}/>
                     <textarea placeholder="Enter your Message..." rows="3" value={message} onChange={(e) => setMessage(e.target.value)}></textarea>
-                    <button type="button"><p className="heading flexItems">Send Message</p></button>
+                    <button type="button"><p className="heading flexItems" onClick={() => preSendEmail()}>Send Message</p></button>
+                    {errorMsg ? <p className="subheading">{errorMsg}</p> : <div style={{display:"none"}}></div>}
                 </form>
             </div>
         </div>
--- src/Components/SmallContactForm.jsx
+++ src/Components/SmallContactForm.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import SendEmail from "../Functions/emailFunctions"
 
 function SmallContactForm({order}){
 
@@ -7,6 +8,18 @@ function SmallContactForm({order}){
     const [phoneNmb,setPhoneNmb] = useState("")
     const [title,setTitle] = useState("")
     const [message,setMessage] = useState("")
+    const [errorMsg,setErrorMsg] = useState("")
+
+    function preSendEmail(){
+        setErrorMsg(SendEmail(name,email,phoneNmb,title,message))
+
+        //Resetting the values of the inputs
+        setName("")
+        setEmail("")
+        setPhoneNmb("")
+        setTitle("")
+        setMessage("")
+    }
 
     return(
         <div className={"smallContactForm flexDisplay column heightGap " + order}>
@@ -14,11 +27,12 @@ function SmallContactForm({order}){
             <p className="bigSubheading">Please Contact us below if you have any queries about the services we provide or would like a Free, No Obligation Quote</p>
             <form className="flexDisplay column gap" action="#">
                 <input type="text" placeholder="Enter your Name..." value={name} onChange={(e) => setName(e.target.value)}/>
-                <input type="text" placeholder="Enter your Email..." value={email} onChange={(e) => setEmail(e.target.value)}/>
+                <input type="email" placeholder="Enter your Email..." value={email} onChange={(e) => setEmail(e.target.value)}/>
                 <input type="text" placeholder="Enter your Phone Number..." value={phoneNmb} onChange={(e) => setPhoneNmb(e.target.value)}/>
                 <input type="text" placeholder="Enter the Email Title..." value={title} onChange={(e) => setTitle(e.target.value)}/>
                 <textarea placeholder="Enter your Message..." rows="3" value={message} onChange={(e) => setMessage(e.target.value)}></textarea>
-                <button type="button"><p className="heading flexItems">Send Message</p></button>
+                <button type="button"><p className="heading flexItems" onClick={() => preSendEmail()}>Send Message</p></button>
+                {errorMsg ? <p className="subheading">{errorMsg}</p> : <div style={{display:"none"}}></div>}
             </form>
         </div>
     )
